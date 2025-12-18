bd4445b24ea6bedc896b0eaeb044318d2b2332f4
mdshahariarhafizofficial <shahariar.works@gmail.com>
2025-12-18 12:56:15 +0600

    create user table

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,32 @@ import {Pool} from "pg";
 const app = express()
 const port = 5000;
 
+
+// parser
+app.use(express.json());
+
+// Pool
 const pool = new Pool({
   connectionString: `postgresql://neondb_owner:npg_gqV2U9lDuOCx@ep-cold-wildflower-ah278kpo-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
 });
 
-// parser
-app.use(express.json());
+
+const initDB = async () => {
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(11),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      )
+    `);
+};
+
+initDB();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Next Dev')
