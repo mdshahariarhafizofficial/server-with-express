355457d9e7257e73675a9497f7027047b831ec0e
mdshahariarhafizofficial <shahariar.works@gmail.com>
2026-01-05 21:36:30 +0600

    todos get & post api added

--- src/server.ts
+++ src/server.ts
@@ -197,6 +197,51 @@ app.delete('/users/:id', async (req: Request, res: Response) => {
   }
 });
 
+// Todos Crud
+app.post('/todos', async (req: Request, res: Response) => {
+  const {user_id, title} = req.body;
+  try {
+    
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Todo inserted",
+      data: result.rows[0]
+    });
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+});
+
+app.get('/todos', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM todos`
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Todos retrieved successfully",
+      data: result.rows
+    })
+  }
+
+  catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
