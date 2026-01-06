48ab70a2b65d1a2f74296dd6dd4737cbd148a1a7
mdshahariarhafizofficial <shahariar.works@gmail.com>
2026-01-06 14:03:10 +0600

    modules file added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import config from "./config";
 import initDB, { pool } from "./config/db";
+import { userRoutes } from "./modules/user/user.routes";
 
 
 const app = express()
@@ -20,52 +21,53 @@ app.get('/', (req: Request, res: Response) => {
 
 
 // Users API
+app.use("/users", userRoutes);
 
-app.post('/users', async (req:Request, res: Response) => {
-    const {name, email} = req.body;
+// app.post('/users', async (req:Request, res: Response) => {
+//     const {name, email} = req.body;
 
-    try {
-      const result = await pool.query(
-        `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
-        [name,  email]
-      ) 
+//     try {
+//       const result = await pool.query(
+//         `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+//         [name,  email]
+//       ) 
 
-      // console.log(result.rows[0]);
-      res.status(201).json({
-          success: true,
-          message: "Data inserted successfully",
-          data: result.rows[0],
-      })
+//       // console.log(result.rows[0]);
+//       res.status(201).json({
+//           success: true,
+//           message: "Data inserted successfully",
+//           data: result.rows[0],
+//       })
 
-    } catch (error: any) {
-      res.status(500).json({
-          success: false,
-          message: error.message,
-      })      
-    }
+//     } catch (error: any) {
+//       res.status(500).json({
+//           success: false,
+//           message: error.message,
+//       })      
+//     }
 
-})
+// })
 
 // Get Api
-app.get('/users', async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(
-      `SELECT * FROM users`
-    );
+// app.get('/users', async (req: Request, res: Response) => {
+//   try {
+//     const result = await pool.query(
+//       `SELECT * FROM users`
+//     );
 
-    res.status(200).json({
-      success: true,
-      message: "Users retrieved successfully",
-      data: result.rows,
-    });
+//     res.status(200).json({
+//       success: true,
+//       message: "Users retrieved successfully",
+//       data: result.rows,
+//     });
 
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    })
-  }
-})
+//   } catch (error: any) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     })
+//   }
+// })
 
 // Get single user
 app.get('/users/:id', async (req: Request, res: Response) => {
