4327a789ab547510983ce6d58b966d9de10992a1
mdshahariarhafizofficial <shahariar.works@gmail.com>
2026-01-09 12:02:59 +0600

    set user in req

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,21 +1,26 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 const auth = () => {
     return (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) {
-            return res.status(500).json({
-                message: "Unauthorized access!"
+        try {
+            const token = req.headers.authorization?.split(" ")[1];
+            if (!token) {
+                return res.status(500).json({
+                    message: "Unauthorized access!"
+                })
+            }
+    
+            const decoded = jwt.verify(token, config.jwt_secret as string);
+            req.user = decoded as JwtPayload;
+            next()        
+        } catch (error: any) {
+            res.status(500).json({
+                status: false,
+                message: error.message
             })
         }
-
-        const decoded = jwt.verify(token, config.jwt_secret as string);
-        console.log(decoded);
-        
-        
-        next()
     }
 };
 
